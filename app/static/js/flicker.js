0e9bda27250ff058b3bd8f61003d090a873cb963
Prasetya <prasetya.career@gmail.com>
2025-11-21 15:48:16 +0700

    m

--- app/static/js/flicker.js
+++
@@ -1,9 +0,0 @@
-// Anti-flicker: jalankan sebelum rendering HTML
-const theme = localStorage.getItem("theme");
-if (theme === "dark") {
-    document.documentElement.classList.add("dark");
-} 
-
-else {
-    document.documentElement.classList.remove("dark");
-}
